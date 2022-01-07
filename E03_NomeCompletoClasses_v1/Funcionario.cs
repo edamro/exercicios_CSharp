1e9ee0254ecdba877a3137b4400599f697311621
edamro <75267837+edamro@users.noreply.github.com>
2022-01-07 07:18:06 -0300

    Add files via upload

---
+++ E03_NomeCompletoClasses_v1/Funcionario.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace FuncionarioDadosPessoais
+{
+
+    class Funcionario
+    {
+
+        #region Properties
+        public string PrimeiroNome { get; set; }
+        public string NomeMeio { get; set; }
+        public string UltimoNome { get; set; }
+        public string NomeCompleto { get; set; }
+        #endregion
+
+        #region Methods
+        public void LerPrimeiroNome()
+        {
+            Console.Write("Primeiro nome: ");
+            PrimeiroNome = Console.ReadLine();
+        }
+
+        public void LerNomeMeio()
+        {
+
+            Console.Write("Nome do meio: ");
+            NomeMeio = Console.ReadLine();
+
+        }
+
+        public void LerUltimoNome()
+        {
+
+            Console.Write("Último nome: ");
+            UltimoNome = Console.ReadLine();
+
+        }
+
+        public void GerarNomeCompleto()
+        {
+
+            if (!string.IsNullOrEmpty(PrimeiroNome) && !string.IsNullOrEmpty(NomeMeio) && !string.IsNullOrEmpty(UltimoNome))
+            {
+                NomeCompleto = $"{PrimeiroNome} {NomeMeio} {UltimoNome}";
+            }
+            else if (!string.IsNullOrEmpty(PrimeiroNome) && string.IsNullOrEmpty(NomeMeio) && string.IsNullOrEmpty(UltimoNome))
+            {
+                NomeCompleto = PrimeiroNome;
+            }
+            else if (!string.IsNullOrEmpty(PrimeiroNome) && !string.IsNullOrEmpty(NomeMeio) && string.IsNullOrEmpty(UltimoNome))
+            {
+                NomeCompleto = $"{PrimeiroNome} {NomeMeio}";
+            }
+            else if (!string.IsNullOrEmpty(PrimeiroNome) && string.IsNullOrEmpty(NomeMeio) && !string.IsNullOrEmpty(UltimoNome))
+            {
+                NomeCompleto = $"{PrimeiroNome} {UltimoNome}";
+            }
+            else if (string.IsNullOrEmpty(PrimeiroNome) && !string.IsNullOrEmpty(NomeMeio) && string.IsNullOrEmpty(UltimoNome))
+            {
+                NomeCompleto = NomeMeio;
+            }
+            else if (string.IsNullOrEmpty(PrimeiroNome) && !string.IsNullOrEmpty(NomeMeio) && !string.IsNullOrEmpty(UltimoNome))
+            {
+                NomeCompleto = $"{NomeMeio} {UltimoNome}";
+            }
+            else if (string.IsNullOrEmpty(PrimeiroNome) && string.IsNullOrEmpty(NomeMeio) && !string.IsNullOrEmpty(UltimoNome))
+            {
+                NomeCompleto = UltimoNome;
+            }
+            else
+            {
+                NomeCompleto = "<sem nome>";
+            }
+
+        }
+
+        public void MostrarNomeCompleto()
+        {
+
+            Console.WriteLine($"\nNome completo: {NomeCompleto}\n\n");
+
+        }
+        #endregion
+
+    }
+
+}
---
+++ E03_NomeCompletoClasses_v1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FuncionarioDadosPessoais
+{
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            Funcionario funcionario01 = new Funcionario();
+
+            funcionario01.LerPrimeiroNome();
+            funcionario01.LerNomeMeio();
+            funcionario01.LerUltimoNome();
+            funcionario01.GerarNomeCompleto();
+            funcionario01.MostrarNomeCompleto();
+
+            Console.ReadKey();
+
+        }
+
+    }
+
+}
